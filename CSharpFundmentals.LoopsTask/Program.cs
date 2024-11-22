d79a1cbc78029255a5348641a1237d6b1d7b4e55
Adel Kamal <adelkamal911@gmail.com>
2024-11-22 10:08:58 +0200

    loops training

---
+++ CSharpFundmentals.LoopsTask/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace CSharpFundmentals.LoopsTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter 5 numbers");
+            int[] numbers = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"Enter number {i + 1}: ");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            Array.Sort(numbers);
+
+            Console.WriteLine("Numbers after sorting are (for): ");
+            for (int i = 0; i <numbers.Length;i++)
+            {
+                Console.WriteLine(numbers[i]);
+
+            }
+
+            Console.WriteLine("Numbers after sorting are (foreach): ");
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ CSharpFundmentals.LoopsTraining/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace CSharpFundmentals.LoopsTraining
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many numbers you want to add?");
+            int length = int.Parse(Console.ReadLine());
+
+            int[] nummbers = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write($"write the number {i +1} : ");
+                nummbers[i] = int.Parse(Console.ReadLine());
+            }
+            
+            Array.Reverse(nummbers);
+            Console.WriteLine("after reversal: ");
+            foreach (int nummber in nummbers)
+                Console.WriteLine($"number is: {nummber}");
+
+
+            Console.WriteLine($"avairage is: {nummbers.Average()}");
+
+
+
+        }
+    }
+}
