529b73b5840de425e7a3403a999e52c11abe676f
Adel Kamal <adelkamal911@gmail.com>
2024-11-22 10:12:32 +0200

    Arithmetic operations and operation precedence

--- CSharpFundmentals.ArithmeticOperations/Program.cs
+++ CSharpFundmentals.ArithmeticOperations/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+            int x = 25;
+            int y = 10;
+            
+            // var is Implicit data type
+            var z =(double) x / y;
+            //or
+            double x1 = 25;
+            int y1 = 10;
+            //or
+            int x2 = 25;
+            double y2 = 10;
+
+            Console.WriteLine($"x + y = {x + y}"); // addition
+            Console.WriteLine($"x - y = {x - y}"); // substraction
+            Console.WriteLine($"x * y = {x * y}"); // multiblication
+            Console.WriteLine($"(int) x / (int) y = {x / y} (wrong)"); // division
+            Console.WriteLine($"(double) x1 / (int) y1 = {x1 / y1} (correct)"); // division
+            Console.WriteLine($"(int) x1 / (double) y1 = {x2 / y2} (correct)"); // division
+            Console.WriteLine($"(double) z =(double) x / y; z =  {z} (correct)"); // division
+            Console.WriteLine($"x % y = {x % y}"); // reminder
         }
     }
 }
---
+++ CSharpFundmentals.OperatorPrecedence/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace CSharpFundmentals.DebuggerBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter a number: ");
+             string number = Console.ReadLine() ?? "";
+            int parsedNumber = int.Parse(number);
+            int reminder = parsedNumber % 2;
+
+            if (parsedNumber == 0)
+            {
+                Console.WriteLine("Zero is neither even nor odd.");
+            }
+            else if (reminder == 1)
+            {
+                Console.WriteLine($"{number} is odd.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is even.");
+
+            }
+        }
+    }
+}
